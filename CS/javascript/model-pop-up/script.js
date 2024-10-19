0b91b60485f6d03c93253e42c93be2af4d473305
Devin <devin.witten@hgs.hiddengeniusproject.org>
2024-10-19 12:02:50 -0700

    Update script.js

--- CS/javascript/model-pop-up/script.js
+++ CS/javascript/model-pop-up/script.js
@@ -1,3 +1,25 @@
 function modelPopUp() {
-    // Your code here.
+    // Your coddocument.addEventListener("DOMContentLoaded", () => {
+    
+    const modal = document.getElementById("myModal");
+    const openModalButton = document.getElementById("openModal");
+    const closeModalButton = document.getElementById("closeModal");
+
+  
+    openModalButton.addEventListener("click", () => {
+        modal.style.display = "block";
+    });
+
+   
+    closeModalButton.addEventListener("click", () => {
+        modal.style.display = "none";
+    });
+
+   
+    window.addEventListener("click", (event) => {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    });
+});e here.
 }   
