a38f875350c446cdee208e2efd87115c5ba27cb3
Devin <devin.witten@hgs.hiddengeniusproject.org>
2024-12-04 18:02:36 -0800

    Update intro-to-flask.py

--- CS/flask/Intro-to-Flask/intro-to-flask.py
+++ CS/flask/Intro-to-Flask/intro-to-flask.py
@@ -1,1 +1,9 @@
-# instruction will below. 
+# Import the flask class from the flask library 
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return "Hello World"
+
+if __name__ == '__main__':
+    app.run(debug = True)# instruction will below. 
